bc7b3f02baf429934737e35fde0b02461c2c6841
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-09-02 17:41:41 +0600

    dashboard front end designed

---
+++ src/java/com/pharmacy/product/Medicine.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pharmacy.product;
+
+/**
+ *
+ * @author rktirtho
+ */
+public class Medicine {
+    
+    private int id;
+    private String name;
+    private String descprition;
+    private String group; // Peracitamole
+    private String type;  // tablet, Capsule, Injection etc
+    private MedicineAuthor author;
+    private double unitSize;
+    private double unitSellingPrize;
+    private double unitBuyingPrize;
+    private double discount;
+    private boolean isAvailable;
+    private int quantity;
+
+    
+    
+}
