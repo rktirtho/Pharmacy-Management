c5c1fa46bfd3588d38748af88e7823202c7ece9a
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-09-02 15:25:35 +0600

    initial commit with library file

---
+++ src/java/com/pharmacy/admin/Admin.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pharmacy.admin;
+
+/**
+ *
+ * @author rktirtho
+ */
+public class Admin {
+    
+    private String name;
+    private String userName;
+    private String password;
+    private String acc_type;
+    private boolean isActive;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAcc_type() {
+        return acc_type;
+    }
+
+    public void setAcc_type(String acc_type) {
+        this.acc_type = acc_type;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    
+    
+    
+}
