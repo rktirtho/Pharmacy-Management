46e41fdcbc36a235e40eaf3173b25d41d430a820
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-09-08 16:36:45 +0600

    sell page js added

---
+++ web/res/js/sell-handaler.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(function () {
+    '<tr>'
+            + '<td ><input list="name-m" class="item-entry" id="" style="width: 380px" type="text" name="" /></td>'
+            + '<td></td>'
+            + '<td><input type="number" name="" /></td>'
+            + '<td></td>'
+            + '<td></td>'
+            + '<td><a class="btn btn-danger" c>Remove</a></td>'
+            + '</tr>)'
+
+
+});
+
+
